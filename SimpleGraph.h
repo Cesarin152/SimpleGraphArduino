8bdba5c7ee00c2a0d5a92b78763f8ecb44728e82
Cesarin152 <65696981+Cesarin152@users.noreply.github.com>
2024-07-21 12:45:28 -0500

    SimpleGraph.h

--- SimpleGraph.h
+++ SimpleGraph.h
@@ -6,6 +6,8 @@
 class SimpleGraph {
 public:
     SimpleGraph(U8G2 &display);
+    void setSubplot(int rows, int cols, int index);
+    void resetRegion();
     void drawAxes();
     void drawPoint(int x, int y);
     void drawLine(int x1, int y1, int x2, int y2);
@@ -17,6 +19,10 @@ public:
 
 private:
     U8G2 &u8g2;
+    int regionX, regionY, regionWidth, regionHeight;
+    int rows, cols, currentIndex;
+    int mapX(int x);
+    int mapY(int y);
 };
 
 #endif
